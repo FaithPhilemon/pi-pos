746c688e4a4247cd9c15c948f63ea828f982f387
Faith <philemonfaith@gmail.com>
2023-11-16 02:37:00 +0100

    modify controller

--- app/Http/Controllers/Admin/ProductsController.php
+++ app/Http/Controllers/Admin/ProductsController.php
@@ -30,7 +30,9 @@ class ProductsController extends Controller
             $pageTitle = Category::where('id', $request->input('type'))->first()->name;
         }
 
-        $products = $query->get();
+        // $products = $query->get();
+        $products = $query->paginate(20); // 10 items per page,
+
 
         return view('products.index', compact('products', 'categories', 'stores', 'pageTitle'));
     }
@@ -49,18 +51,19 @@ class ProductsController extends Controller
         $this->authorize('create products');
 
         $validator = Validator::make($request->all(), [
-            'name' => 'required',
-            'author' => 'nullable',
-            'ISBN' => 'nullable',
-            'description' => 'nullable',
-            'stock' => 'nullable|integer',
-            'alert_quantity' => 'nullable|integer',
-            'manage_stock' => 'nullable|boolean',
-            'price' => 'numeric',
-            'image' => 'image|max:500', // Max 500KB image size
-            'category_id' => 'nullable|exists:categories,id',
-            'contact_id' => 'nullable|exists:contacts,id',
-            'store_id' => 'nullable|exists:stores,id',
+            'name'              => 'required',
+            'author'            => 'nullable',
+            'ISBN'              => 'nullable',
+            'description'       => 'nullable',
+            'stock'             => 'nullable|integer',
+            'alert_quantity'    => 'nullable|integer',
+            'manage_stock'      => 'nullable|boolean',
+            'price'             => 'numeric',
+            'image'             => 'image|max:500', // Max 500KB image size
+            'category_id'       => 'nullable|exists:categories,id',
+            'subcategory_id'    => 'nullable|exists:categories,parent_category_id',
+            'contact_id'        => 'nullable|exists:contacts,id',
+            'store_id'          => 'nullable|exists:stores,id',
         ]);
 
         if ($validator->fails()) {
@@ -68,17 +71,18 @@ class ProductsController extends Controller
         }
 
         $product = new Product();
-        $product->name = $request->input('name');
-        $product->author = $request->input('author');
-        $product->ISBN = $request->input('ISBN');
-        $product->description = $request->input('description');
-        $product->stock = $request->input('stock');
-        $product->alert_quantity = $request->input('alert_quantity');
-        $product->manage_stock = $request->input('manage_stock', 0);
-        $product->price = $request->input('price');
-        $product->category_id = $request->input('category_id');
-        $product->contact_id = $request->input('contact_id');
-        $product->store_id = $request->input('store_id');
+
+        $product->name              = $request->input('name');
+        $product->author            = $request->input('author');
+        $product->ISBN              = $request->input('ISBN');
+        $product->description       = $request->input('description');
+        $product->stock             = $request->input('stock');
+        $product->alert_quantity    = $request->input('alert_quantity');
+        $product->manage_stock      = $request->input('manage_stock', 0);
+        $product->price             = $request->input('price');
+        $product->category_id       = $request->input('category_id');
+        $product->contact_id        = $request->input('contact_id');
+        $product->store_id          = $request->input('store_id');
 
         if ($request->hasFile('image')) {
             $image = $request->file('image');
@@ -105,35 +109,37 @@ class ProductsController extends Controller
         $this->authorize('edit products');
 
         $validator = Validator::make($request->all(), [
-            'name' => 'required',
-            'author' => 'nullable',
-            'ISBN' => 'nullable',
-            'description' => 'nullable',
-            'stock' => 'nullable|integer',
-            'alert_quantity' => 'nullable|integer',
-            'manage_stock' => 'nullable|boolean',
-            'price' => 'numeric',
-            'image' => 'image|max:500', // Max 500KB image size
-            'category_id' => 'nullable|exists:categories,id',
-            'contact_id' => 'nullable|exists:contacts,id',
-            'store_id' => 'nullable|exists:stores,id',
+            'name'              => 'required',
+            'author'            => 'nullable',
+            'ISBN'              => 'nullable',
+            'description'       => 'nullable',
+            'stock'             => 'nullable|integer',
+            'alert_quantity'    => 'nullable|integer',
+            'manage_stock'      => 'nullable|boolean',
+            'price'             => 'numeric',
+            'image'             => 'image|max:500', // Max 500KB image size
+            'category_id'       => 'nullable|exists:categories,id',
+            'subcategory_id'    => 'nullable|exists:categories,parent_category_id',
+            'contact_id'        => 'nullable|exists:contacts,id',
+            'store_id'          => 'nullable|exists:stores,id',
         ]);
 
         if ($validator->fails()) {
             return redirect()->route('products.edit', $product->id)->withErrors($validator)->withInput();
         }
 
-        $product->name = $request->input('name');
-        $product->author = $request->input('author');
-        $product->ISBN = $request->input('ISBN');
-        $product->description = $request->input('description');
-        $product->stock = $request->input('stock');
-        $product->alert_quantity = $request->input('alert_quantity');
-        $product->manage_stock = $request->input('manage_stock', 0);
-        $product->price = $request->input('price');
-        $product->category_id = $request->input('category_id');
-        $product->contact_id = $request->input('contact_id');
-        $product->store_id = $request->input('store_id');
+        $product->name              = $request->input('name');
+        $product->author            = $request->input('author');
+        $product->ISBN              = $request->input('ISBN');
+        $product->description       = $request->input('description');
+        $product->stock             = $request->input('stock');
+        $product->alert_quantity    = $request->input('alert_quantity');
+        $product->manage_stock      = $request->input('manage_stock', 0);
+        $product->price             = $request->input('price');
+        // $product->category_id    = $request->input('category_id');
+        $product->category_id       = $request->input('subcategory_id');
+        $product->contact_id        = auth()->user()->id;
+        $product->store_id          = $request->input('store_id');
 
         if ($request->hasFile('image')) {
             // Delete the old image
