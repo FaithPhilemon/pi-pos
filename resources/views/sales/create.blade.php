9e27140e6968d848fec7d3eabcb3495fe1f952d0
Faith <philemonfaith@gmail.com>
2024-04-18 03:29:03 +0100

    refix form

--- resources/views/sales/create.blade.php
+++ resources/views/sales/create.blade.php
@@ -175,7 +175,7 @@
                                         <tfoot>
                                             <tr class="text-bold">
                                                 <td colspan="4">Grand Total</td>
-                                                <td><span id="grand_total">₦0.00</span></td>
+                                                <td><h5><span id="grand_total">₦0.00</span></h5></td>
                                                 <td></td>
                                             </tr>
                                             <tr id="discount_row">
@@ -203,6 +203,11 @@
                                             </tr> --}}
                                         </tfoot>
                                     </table>
+                                </div>
+
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-dismiss="modal">{{ __('Close')}}</button>
+                                    <button type="submit" class="btn btn-primary">{{ __('Save')}}</button>
                                 </div>
                         </form>
                         
